c4a3ad0f116bb63b8ac8068945fafaa461ba0df3
unknown <prietokenneth28@gmail.com>
2026-01-20 18:43:32 -0500

    prueba

--- backend/src/middlewares/cors.middleware.js
+++ backend/src/middlewares/cors.middleware.js
@@ -2,7 +2,6 @@ import cors from "cors";
 
 const corsOptions = {
     origin: (origin, callback) => {
-
         if (!origin) return callback(null, true);
 
         const isDev = process.env.NODE_ENV === "development";
@@ -12,8 +11,11 @@ const corsOptions = {
             process.env.FRONTEND_DEV_ALT
         ];
 
+        // MODIFICACIÓN AQUÍ: Agregamos FRONTEND_DEV a producción también
         const allowedOriginsProd = [
-            process.env.FRONTEND_PROD 
+            process.env.FRONTEND_PROD,
+            "http://127.0.0.1:5500", // Tu IP local (Live Server)
+            "http://localhost:5500"   // Alternativa local
         ];
 
         const allowedOrigins = isDev
@@ -23,7 +25,8 @@ const corsOptions = {
         if (allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
-            callback(null, false); // 👈 evita "Failed to fetch"
+            console.log("Bloqueado por CORS:", origin); // Útil para depurar en los logs de Render
+            callback(null, false);
         }
     },
     credentials: true,
